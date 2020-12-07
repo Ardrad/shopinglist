b496733cae920f763fd23390f55c1b465b981089
Tamara Kurek <patryk1.stefan@gmail.com>
2020-12-07 20:36:58 +0100

    make simple styles

--- shoping-list/src/components/Header.tsx
+++ shoping-list/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {AppBar} from "@material-ui/core";
+import {AppBar, Container} from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
@@ -7,9 +7,11 @@ export const Header=()=>{
     return(
         <AppBar position="static">
             <Toolbar>
+                <Container maxWidth={'md'}>
                 <Typography variant={"h6"}>
                 Shopping-list
                 </Typography>
+                </Container>
             </Toolbar>
         </AppBar>
     )
--- shoping-list/src/components/NewItemForm.tsx
+++ shoping-list/src/components/NewItemForm.tsx
@@ -1,5 +1,61 @@
 import React from "react";
+import {
+    Button,
+    Container, createStyles,
+    FormGroup,
+    InputLabel,
+    makeStyles,
+    MenuItem,
+    Paper,
+    Select,
+    TextField, Theme
+} from "@material-ui/core";
+
 
 export const NewItemForm=()=>{
-    return null
+    const styles = makeStyles((theme:Theme)=>
+         createStyles({
+            form:{
+                display:'flex',
+                justifyContent:'space-between',
+                alignItems:'center',
+                flexWrap:'wrap'
+            },
+             field:{
+                margin:'10px 20px'
+             },
+             unitSelect:{
+                minWidth:'100px'
+             }
+        })
+
+    );
+    const classes =styles();
+    return (
+        <Paper square elevation={3}>
+            <Container maxWidth={"md"}>
+                <form className={classes.form}>
+                    <FormGroup className={classes.field}>
+                        <InputLabel>Item</InputLabel>
+                        <TextField/>
+                    </FormGroup>
+                    <FormGroup className={classes.field}>
+                        <InputLabel>Quantity</InputLabel>
+                        <TextField/>
+                    </FormGroup>
+                    <FormGroup className={classes.field}>
+                        <InputLabel>Unit</InputLabel>
+                        <Select className={classes.unitSelect}>
+                            <MenuItem value={"g"}>grams</MenuItem>
+                            <MenuItem value={"kg"}>kilograms</MenuItem>
+                            <MenuItem value={"item"}>item(s)</MenuItem>
+                        </Select>
+
+                    </FormGroup>
+                    <Button variant="contained" color="primary">Add</Button>
+                </form>
+            </Container>
+        </Paper>
+
+    );
 }
